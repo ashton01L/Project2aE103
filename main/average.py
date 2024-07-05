bfa869422bc82be84f9c750669be74706f11f53d
ashton01L <ashtonamazing@gmail.com>
2024-07-04 22:37:07 -0500

    created input request and variables for inputs, still working on average

---
+++ main/average.py
@@ -0,0 +1,18 @@
+# Author: Ashton Lee
+# Github User: ashton01L
+# Date: 7/4/2024
+# Description: Asks the user for five numbers and then prints out the average of those numbers.
+
+# unfinished
+
+print("Please enter five numbers:")
+num1 = input()
+num2 = input()
+num3 = input()
+num4 = input()
+num5 = input()
+
+inpList = [num1, num2, num3, num4, num5]
+average = sum(inpList)/len(inpList)
+
+print(average)
